7be474f8cac6cca4dc165e5d9d26bc8189318ecc
olenadm <olenad@gmail.com>
2024-02-10 17:14:40 +0100

    Replaced capitalisation of H1 with a simple css rule

--- hacker-news-nextjs-fm/src/components/Stories.tsx
+++ hacker-news-nextjs-fm/src/components/Stories.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Stories({ stories, page, limit, type }) {
   return (
     <>
-      <h1 className="mt-5 mb-5">{`${type?.slice(0,1).toUpperCase()}${type?.slice(1)}`} Strories</h1>
+      <h1 className="mt-5 mb-5">{type} Strories</h1>
       <ul>
         {stories.map(({ id, title, url }) => (
           <li key={id}>
