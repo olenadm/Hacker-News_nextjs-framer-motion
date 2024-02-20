3b3c8ade6f593f2c911ccdec9c15dd029dc441f1
olenadm <olenad@gmail.com>
2024-02-20 11:39:26 +0100

    Stories div and background animation fix

--- hacker-news-nextjs-fm/src/components/Stories.tsx
+++ hacker-news-nextjs-fm/src/components/Stories.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import { Stories } from "@/pages/api/types";
 import Link from "next/link";
 
-export default function Stories({ stories,  type }) {
+export default function Stories({ stories, type }) {
   return (
     <>
       <h1 className="mb-4 mt-lg-5 mb-lg-5">{type} Strories</h1>
-      <ol>
-        {stories.map(({ id, title, url }) => (
-          <li key={id}>
-            {url ? (
-              <Link href={url}>
-                <h5 key={id}>{id} {title}</h5>
-              </Link>
-            ) : (
-              <h5 key={id}>{title}</h5>
-            )}
-          </li>
+      <div className="row">
+        {stories.map(({ id, title, url }, index: number) => (
+          <div className="col-sm-6" key={id}>
+            <div className="story">
+              {url ? (
+                <Link href={url}>
+                  <h5 key={id}> {title}</h5>
+                </Link>
+              ) : (
+                <h5 key={id}>{title}</h5>
+              )}
+            </div>
+          </div>
         ))}
-      </ol>
-      
+      </div>
     </>
   );
 }
